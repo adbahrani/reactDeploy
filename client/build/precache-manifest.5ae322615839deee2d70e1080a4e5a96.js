f427f61a7effb669c31c0e5ffb5d066e2d2a9ad9
Adam Bahrani <adbarani2@gmail.com>
2020-10-17 09:48:59 -0400

    Push

---
+++ client/build/precache-manifest.5ae322615839deee2d70e1080a4e5a96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376640bba52838e420ee80325ad09d47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5b888e6898720db8002",
+    "url": "/static/css/main.1ef92e19.chunk.css"
+  },
+  {
+    "revision": "5309e449abb858676493",
+    "url": "/static/js/2.7f4868fe.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.7f4868fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b888e6898720db8002",
+    "url": "/static/js/main.c3cd15ae.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
