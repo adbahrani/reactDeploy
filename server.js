b047fa3f333a6d8f1255b203f4c3980f719992f8
Adam Bahrani <adbarani2@gmail.com>
2020-10-17 09:38:27 -0400

    Update

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI ||
   "mongodb+srv://abahrani:5432947Ab@cluster0.s7ehm.mongodb.net/reactreadinglist?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
